1ae56bbb75cf2a6d39c8b7a61c5531bdb0d7771d
chaser-man <95667028+chaser-man@users.noreply.github.com>
2025-04-17 16:31:10 -0600

    new Strat

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+""" 
 import time
 import turtle
 
@@ -12,13 +13,13 @@ t1.speed(0)
 t2 = turtle.Turtle()
 t2.hideturtle()
 t2.width(10)
-t2.color("yellow")
+t2.color("blue")
 t2.backward(50)
 
 t3 = turtle.Turtle()
 t3.hideturtle()
 t3.width(10)
-t3.color("blue")
+t3.color("yellow")
 
 
 
@@ -61,14 +62,6 @@ def move_right():
 def test():
     print("nothing")
 
-def eraseHorizontal():
-    t2.seth(t1.heading())
-    t2.forward(1)
-
-def eraseVertical(): #finished here
-    t3.penup()
-    t3.speed(0)
-    t3.goto(t1.position())
 
 screen.listen()
 screen.onkey(move_up, "Up")
@@ -76,10 +69,108 @@ screen.onkey(move_down, "Down")
 screen.onkey(move_left, "Left")
 screen.onkey(move_right, "Right")
 
+def eraseHorizontal():
+    t2.seth(t1.heading())
+    t2.forward(2)
+
+    global direction
+    t3.speed(0)
+    if direction == "up":
+        t3.pendown()
+        t3.goto(t1.xcor(), t1.ycor() - 50)
+
+
 
 while True:
-    t1.forward(1)
+    t1.forward(2)
     screen.ontimer(eraseHorizontal,1)
 
+
+turtle.done()
+"""
+
+import turtle
+import random
+
+#random_color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)  # You can also let it default
+
+# e.g. positions from –280 up to +280 in steps of 20
+apple_positions = list(range(-280, 281, 20))
+
+
+snake_segments = []
+
+def add_segment(position):
+    segment = turtle.Turtle("square")
+    segment.color("green")
+    segment.penup()
+    segment.goto(position)
+    snake_segments.append(segment)
+
+add_segment((0, 0)) # first 3 segments
+add_segment((-20,0))
+add_segment((-40,0))
+
+direction = "right" # initial directionS
+
+def move_snake():
+    # Move segments from tail to head
+    for i in range(len(snake_segments) - 1, 0, -1):
+        new_x = snake_segments[i - 1].xcor()
+        new_y = snake_segments[i - 1].ycor()
+        snake_segments[i].goto(new_x, new_y)
+    
+    # Move the head
+    head = snake_segments[0]
+    if direction == "up":
+        head.sety(head.ycor() + 20)
+    elif direction == "down":
+        head.sety(head.ycor() - 20)
+    elif direction == "left":
+        head.setx(head.xcor() - 20)
+    elif direction == "right":
+        head.setx(head.xcor() + 20)
+
+def move_up():
+    global direction
+    if direction != "down":
+        direction = "up"
+def move_down():
+    global direction
+    if direction != "up":
+        direction = "down"
+
+def move_left():
+    global direction
+    if direction != "right":
+        direction = "left"
+
+def move_right():
+    global direction
+    if direction != "left":
+        direction = "right"
+
+
+
+screen.listen()
+screen.onkey(move_up, "Up")
+screen.onkey(move_down, "Down")
+screen.onkey(move_left, "Left")
+screen.onkey(move_right, "Right")
+
+apple = turtle.Turtle("square")
+apple.speed(0)
+apple.penup()
+apple.color("red")
+apple.goto(random.choice(apple_positions), random.choice(apple_positions))
+
+
+#if segment
+
+while True:
+    move_snake()
 
 turtle.done()
