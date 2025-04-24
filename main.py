ea0b6fa63ebac48c168815083c0308ddabdda76e
chaser-man <95667028+chaser-man@users.noreply.github.com>
2025-04-24 09:13:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,4 @@
-""" 
-import time
-import turtle
 
-screen = turtle.Screen()
-
-t1 = turtle.Turtle()
-t1.hideturtle()
-t1.width(5)
-t1.color("red")
-t1.speed(0)
-
-t2 = turtle.Turtle()
-t2.hideturtle()
-t2.width(10)
-t2.color("blue")
-t2.backward(50)
-
-t3 = turtle.Turtle()
-t3.hideturtle()
-t3.width(10)
-t3.color("yellow")
-
-
-
-direction = "right"
-
-
-
-def move_up():
-    global direction
-    if direction != "down":
-        direction = "up"
-        t1.speed(0)
-        t1.seth(90)
-        t1.speed(5)
-
-def move_down():
-    global direction
-    if direction != "up":
-        direction = "down"
-        t1.speed(0)
-        t1.seth(270)
-        t1.speed(5)
-
-def move_left():
-    global direction
-    if direction != "right":
-        direction = "left"
-        t1.speed(0)
-        t1.seth(180)
-        t1.speed(5)
-
-def move_right():
-    global direction
-    if direction != "left":
-        direction = "right"
-        t1.speed(0)
-        t1.seth(0)
-        t1.speed(5)
-
-def test():
-    print("nothing")
-
-
-screen.listen()
-screen.onkey(move_up, "Up")
-screen.onkey(move_down, "Down")
-screen.onkey(move_left, "Left")
-screen.onkey(move_right, "Right")
-
-def eraseHorizontal():
-    t2.seth(t1.heading())
-    t2.forward(2)
-
-    global direction
-    t3.speed(0)
-    if direction == "up":
-        t3.pendown()
-        t3.goto(t1.xcor(), t1.ycor() - 50)
-
-
-
-while True:
-    t1.forward(2)
-    screen.ontimer(eraseHorizontal,1)
-
-
-turtle.done()
-"""
 
 import turtle
 import random
@@ -105,10 +16,14 @@ apple = turtle.Turtle("square")
 apple.speed(0)
 apple.penup()
 apple.color("red")
-apple.goto(random.choice(apple_positions), random.choice(apple_positions))
 
 direction = "right" # initial directionS
-
+x_axis = 0
+y_axis = 0
+score = 0
+apple.goto(0,250)
+apple.write(score)
+apple.goto(random.choice(apple_positions), random.choice(apple_positions))
 
 snake_segments = []
 
@@ -144,6 +59,22 @@ def collisions():
     if snake_segments[0].position() == apple.position():
         add_segment(apple.position())
         apple.goto(random.choice(apple_positions), random.choice(apple_positions))
+        
+
+def check_bounds():
+    global x_axis
+    global y_axis
+    if x_axis >= 14 or x_axis <= -16:
+        turtle.bye()
+    if y_axis >= 15 or y_axis <= -15:
+        turtle.bye()
+def check_self_collision():
+    head = snake_segments[0]
+    for segment in snake_segments[1:]:  # exclude the head
+        if head.distance(segment) < 20:
+            print("Collision with self!")
+            turtle.bye()  # Ends the game
+
 
 def move_up():
     global direction
@@ -176,9 +107,20 @@ add_segment((-40,0))
 
 
 while True:
+    if direction == "right":
+        x_axis += 1
+    if direction == "left":
+        x_axis -= 1
+    if direction == "up":
+        y_axis += 1
+    if direction == "down":
+        y_axis -= 1
     screen.tracer(0)
     move_snake()
+    check_bounds()
+    check_self_collision()
     screen.update()
     collisions()
+    
     time.sleep(0.15)
 turtle.done()
